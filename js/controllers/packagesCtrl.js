a5d5925079ec4c92de490e523f8dcc429a9f1eb5
Kellen Pugmire <kepugmire@gmail.com>
2017-05-22 16:26:00 -0600

    final commit

--- js/controllers/packagesCtrl.js
+++ js/controllers/packagesCtrl.js
@@ -1,22 +1,17 @@
 angular.module('devmtnTravel').controller('packagesCtrl', function($scope, mainService, $stateParams){
 
-// console.log($stateParams)
-//     var packs = mainService.packageInfo
-    $scope.packages = mainService.packageInfo
 
-    //     var selectedCountry = []
+    var packs = mainService.packageInfo
+    var selectedCountry = []
 
-    // for (var i = 0; i < packs.length; i++) {
-    //    for (var key in packs[i]) {
-    //     if ($stateParams.country == packs[i][key]) {
-    //         console.log($stateParams.country)
-    //         console.log(packs[i][key])
-    //         selectedCountry.push(packs[i])   
-            
-    //     }
-    //    }
-    // }
-    // $scope.packages = selectedCountry;
-    // console.log($scope.packages)
+    for (var i = 0; i < packs.length; i++) {
+       for (var key in packs[i]) {
+        if ($stateParams.country == packs[i][key]) {
+            selectedCountry.push(packs[i])   
+        }
+       }
+    }
+    $scope.packages = selectedCountry;
+
 
 })
